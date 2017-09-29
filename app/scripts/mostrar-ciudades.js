67fef4170bacf9e80ddc60da5f97dd1dfba96cb4
mel1bt <mberrocal.tipte@gmail.com>
2017-09-28 20:17:16 -0500

    update branch

--- app/scripts/mostrar-ciudades.js
+++ app/scripts/mostrar-ciudades.js
@@ -5,7 +5,7 @@ const mostrarCiudades = (ciudades, contenedorCiudades) => {
     const htmlCiudad = `
         <div class="col-md-6 ciudad">
           <h4>${ciudad.nombre}</h4>
-          <p data-id="2147714"><span>20.5</span>°<img src="http://openweathermap.org/img/w/01n.png" alt=""></p>
+          <p data-id="${ciudad.id}"><span>20.5</span>°<img src="http://openweathermap.org/img/w/01n.png" alt=""></p>
           <p>${ciudad.descripcion}</p>
         </div>`;
 
